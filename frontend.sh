a26e38fedd3d4144d1942182496facd94a40b564
Kiarash Korki <kiarash.96@gmail.com>
2022-07-20 15:17:14 +0430

    Install dependencies in frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec npm --prefix ./frontend run build -- --base=/forms/ --outDir=../backend/src/main/resources/static/forms --emptyOutDir --watch
+npm --prefix ./frontend install
+npm --prefix ./frontend run build -- --base=/forms/ --outDir=../backend/src/main/resources/static/forms --emptyOutDir --watch
